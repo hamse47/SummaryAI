bf32c6f3f0c29ddef782b484055735089db48129
hamza odowa <hamza2.odowa@live.uwe.ac.uk>
2023-11-22 08:51:15 +0000

    ✨ Added comments

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,12 @@ import { store } from "./services/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* 
+      The Provider component from react-redux allows your Redux store to be accessible
+      to any nested components that have been wrapped in the connect() function.
+    */}
     <Provider store={store}>
+      {/* App component is the root component of your application */}
       <App />
     </Provider>
   </React.StrictMode>
